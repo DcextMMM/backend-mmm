deb5a88631c64e23d42c0d5979d636ddf0a46e59
Guilherme Vanderley <guivdouradog@gmail.com>
2022-09-17 02:54:42 -0300

    bind

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -2,32 +2,36 @@ import userSchema from '../schemas/users';
 
 export default class Validate {
   constructor() {
-    const schema = new userSchema();
-    this.cadastro = async (req, res, next) => {
-      try {
-        req.body = await schema.cadastro.validate(req.body, { stripUnknown: true });
-        next();
-      } catch (err) {
-        return res.status(400).json({ error: err.errors });
-      }
-    };
+    this.schema = new userSchema();
+    this.cadastro = this.cadastro.bind(this);
+    this.login = this.login.bind(this);
+    this.update = this.update.bind(this);
+  }
 
-    this.login = async (req, res, next) => {
-      try {
-        req.body = await schema.login.validate(req.body, { stripUnknown: true });
-        next();
-      } catch (err) {
-        return res.status(400).json({ error: err.errors });
-      }
-    };
+  async cadastro(req, res, next) {
+    try {
+      req.body = await this.schema.cadastro.validate(req.body, { stripUnknown: true });
+      next();
+    } catch (err) {
+      return res.status(400).json({ error: err.errors });
+    }
+  }
 
-    this.update = async (req, res, next) => {
-      try {
-        req.body = await schema.update.validate(req.body, { stripUnknown: true });
-        next();
-      } catch (err) {
-        return res.status(400).json({ error: err.errors });
-      }
-    };
+  async login(req, res, next) {
+    try {
+      req.body = await this.schema.login.validate(req.body, { stripUnknown: true });
+      next();
+    } catch (err) {
+      return res.status(400).json({ error: err.errors });
+    }
+  }
+
+  async update(req, res, next) {
+    try {
+      req.body = await this.schema.update.validate(req.body, { stripUnknown: true });
+      next();
+    } catch (err) {
+      return res.status(400).json({ error: err.errors });
+    }
   }
 }
